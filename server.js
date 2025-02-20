91fbef1f32384da0e12fb9eca7affe88c91efdfb
Nuno Xavier <nuno@192.168.0.4>
2025-02-19 21:58:31 -0300

    tirando linhas inuteis do server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import express from 'express'
 const server = express()
 
 server.use(express.static('public'))
-server.use(express.static('public/models'))
 
 server.listen(3333,() => {
     console.log('listen port 3333')
